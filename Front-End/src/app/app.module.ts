a93868929b672a37fe1fbb054c0ef5b5e660fedc
João Guilherme <guilhermesousa5423@gmail.com>
2023-12-03 15:23:17 -0300

    Estrutura inicial da home

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
