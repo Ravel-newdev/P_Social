66fd8300e7c09626e753dd97edd03f3e77e044fc
ViniDeveloper <ljoaovinicius935@gmail.com>
2023-12-05 22:03:19 -0300

    Alteração em Equipamentos (Esqueci)

--- models/Equip.js
+++ models/Equip.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const Equip = new Schema({
-
+    codigo:{
+        type: Number,
+        required:true,
+        unique: true
+       },
     nome:{
         type: String,
         required: true
