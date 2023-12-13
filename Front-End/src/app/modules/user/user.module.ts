ea9f2e4d39cdee9e6669a310b2b039a7ac760f01
Joao Guilherme da Silva Sousa <joao.silva@lead.fortaleza>
2023-12-13 16:40:24 -0300

    feat: Estruturação da tela de reservas

--- Front-End/src/app/modules/user/user.module.ts
+++ Front-End/src/app/modules/user/user.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { CreateRoomComponent } from './pages/create-room/create-room.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReserveComponent } from './pages/reserve/reserve.component';
 
 
 @NgModule({
   declarations: [
-    CreateRoomComponent
+    CreateRoomComponent,
+    ReserveComponent
   ],
   imports: [
     CommonModule,
